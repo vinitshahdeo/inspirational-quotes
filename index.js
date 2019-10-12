3a573bd163123aea574827781220f4277c4facfb
aktsbot <box.ashishk@gmail.com>
2019-10-12 19:54:18 +0530

    Use arr.length than the fixed count in the package
    The package used a fixed 324 in the randomInt() function. It
    is tedious to update the count everytime, one adds a new
    quote.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var arr = require('./data/data.json');
 
-var quote={};
+var size = arr.length;
+var quote = {};
 
-function randomInt(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-module.exports.getRandomQuote = function () { 
-    var index = randomInt(0,324);
+module.exports.getRandomQuote = function () {
+    var index = randomInt(0, size);
     return arr[index].text;
 };
 
-module.exports.getQuote = function (name) { 
-    var index = randomInt(0,324);
-    quote.text=arr[index].text;
-    quote.author=arr[index].from;
+module.exports.getQuote = function (name) {
+    var index = randomInt(0, size);
+    quote.text = arr[index].text;
+    quote.author = arr[index].from;
     return quote;
 };
