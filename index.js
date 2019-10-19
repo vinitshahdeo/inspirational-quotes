137862ba3b4df9223c62a26c42a8bd702fbb41d9
GermaVinsmoke <vaibhav1180@gmail.com>
2019-10-19 22:49:28 +0530

    Added getAuthorQuotes function

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var arr = require('./data/data.json');
 
 var quote = {};
+let authorQuotes = [];
 let len = arr.length - 1;
 
-function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-module.exports.getRandomQuote = function() {
-  var index = randomInt(0, len);
-  return arr[index].text;
-};
+module.exports.getRandomQuote = () => arr[randomInt(0, len)].text;
 
-module.exports.getQuote = function(name) {
+module.exports.getQuote = name => {
   var index = randomInt(0, len);
   quote.text = arr[index].text;
   quote.author = arr[index].from;
   return quote;
+};
+
+module.exports.getAuthorQuotes = name => {
+  for (const quote of arr) {
+    if (name === quote.from) authorQuotes.push(quote);
+  }
+  return authorQuotes;
 };
