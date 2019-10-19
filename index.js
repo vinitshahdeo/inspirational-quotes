cac2cae7677b804803b0088570c3249d758d7e97
GermaVinsmoke <vaibhav1180@gmail.com>
2019-10-19 22:54:11 +0530

    Changed from var to let, const

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var arr = require('./data/data.json');
+const arr = require('./data/data.json');
 
-var quote = {};
+let quote = {};
 let authorQuotes = [];
 let len = arr.length - 1;
 
@@ -9,7 +9,7 @@ randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 module.exports.getRandomQuote = () => arr[randomInt(0, len)].text;
 
 module.exports.getQuote = name => {
-  var index = randomInt(0, len);
+  let index = randomInt(0, len);
   quote.text = arr[index].text;
   quote.author = arr[index].from;
   return quote;
