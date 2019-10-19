ad8904d5c69e99568919fb769b585e6790b75da4
GermaVinsmoke <vaibhav1180@gmail.com>
2019-10-19 22:16:30 +0530

    Added len variable in index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var arr = require('./data/data.json');
 
-var quote={};
+var quote = {};
+let len = arr.length - 1;
 
-function randomInt(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-module.exports.getRandomQuote = function () { 
-    var index = randomInt(0,324);
-    return arr[index].text;
+module.exports.getRandomQuote = function() {
+  var index = randomInt(0, len);
+  return arr[index].text;
 };
 
-module.exports.getQuote = function (name) { 
-    var index = randomInt(0,324);
-    quote.text=arr[index].text;
-    quote.author=arr[index].from;
-    return quote;
+module.exports.getQuote = function(name) {
+  var index = randomInt(0, len);
+  quote.text = arr[index].text;
+  quote.author = arr[index].from;
+  return quote;
 };
