27654b895cca547c80bfd8e369ed25e95f8e4b4e
Ashish Yadav <iloveh4ck1ng@gmail.com>
2019-10-22 12:25:08 +0545

    Dynamic array size value
    Get the size of data so that we don't have to update the max value on randomInt method every time the quotes are added to data.json.

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-var arr = require('./data/data.json');
-
-var quote={};
+const arr = require('./data/data.json');
+// get the size of data so that we don't have to update the max value on randomInt method every time the quotes are added to data.json.
+let size = Object.keys(arr).length;
+let quote={};
 
 function randomInt(min,max)
 {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-module.exports.getRandomQuote = function () { 
-    var index = randomInt(0,324);
+module.exports.getRandomQuote = function () {
+    
+    let index = randomInt(0, size);
     return arr[index].text;
 };
 
 module.exports.getQuote = function (name) { 
-    var index = randomInt(0,324);
+    let index = randomInt(0, size);
     quote.text=arr[index].text;
     quote.author=arr[index].from;
     return quote;
