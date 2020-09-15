1b3be0bb8c7fcf2558351dbf2668a6f9db1cad1f
barunsaraf1@gmail.com <barunsaraf1@gmail.com>
2020-09-15 16:19:50 +0530

    resolved duplicates quotes

--- examples/demo.js
+++ examples/demo.js
@@ -1,5 +1,12 @@
 const quote = require('inspirational-quotes');
+var seen = new Set()
+
+var newQuote = quote.getRandomQuote()
+while(seen.has(newQuote))
+{
+  newQuote = quote.getRandomQuote()
+}
+seen.add(newQuote)
 
 console.log(quote.getQuote());
-
-console.log(quote.getRandomQuote());
+console.log(newQuote) //generating the unvisited random quote
