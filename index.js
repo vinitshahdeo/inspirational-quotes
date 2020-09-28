e88fd42b311f602007410f1b0d8decdc0b23195e
Ederson Ferreira <edersondeveloper@gmail.com>
2020-09-28 15:09:38 -0400

    improving index.js code

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-var arr = require('./data/data.json');
+const arr = require('./data/data.json');
+const QuotesLength = arr.length;
+var quote = {};
 
-var quote={};
-
-function randomInt(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-module.exports.getRandomQuote = function () { 
-    var index = randomInt(0,324);
-    return arr[index].text;
+module.exports.getRandomQuote = function () {
+  var index = randomInt(0, QuotesLength);
+  return arr[index].text;
 };
-
 module.exports.getQuote = function (name) { 
-    var index = randomInt(0,324);
+    var index = randomInt(0, QuotesLength);
     quote.text=arr[index].text;
     quote.author=arr[index].from;
     return quote;
