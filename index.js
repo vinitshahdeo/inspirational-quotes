6689094a52b87b1d9e842b0326365966e6fcb8e8
Vedant Bothikar <vbothikar2001@gmail.com>
2020-10-08 22:08:24 +0530

    Search quote by Author

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var arr = require('./data/data.json');
+var arr = require("./data/data.json");
 
-var quote={};
+var quote = {};
 
-function randomInt(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-module.exports.getRandomQuote = function () { 
-    var index = randomInt(0,324);
-    return arr[index].text;
+module.exports.getRandomQuote = function () {
+  var index = randomInt(0, 324);
+  return arr[index].text;
 };
 
-module.exports.getQuote = function (name) { 
-    var index = randomInt(0,324);
-    quote.text=arr[index].text;
-    quote.author=arr[index].from;
-    return quote;
+module.exports.getQuote = function (name) {
+  var index = randomInt(0, 324);
+  quote.text = arr[index].text;
+  quote.author = arr[index].from;
+  return quote;
+};
+
+module.exports.searchQuote = function (author) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].from.toLowerCase() === author.toLowerCase()) {
+      return arr[i];
+    }
+  }
+  return "Sorry, we do not have any quote from the author ";
 };
