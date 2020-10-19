ef2149e4fa2f79518f06a12289b44d1bad010755
anwar907 <rilltheend@gmail.com>
2020-10-19 23:11:49 +0800

    add quote aspiring software developer

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const quote=require('./index.js');
 
 var Quote = quote.getQuote();
 
-/* getQuote() returns an object containing 'text' and 'author' */
+ getQuote() returns an object containing 'your goals are long but your life is short, so take your time seriously and do it now' and 'anwar, aspiring software developer'
 
 console.log('\n***********************************');
 console.log('***********************************\n\n');
