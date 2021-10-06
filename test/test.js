e81f9433d57c428f70f6b342557fc31ea441a62f
Rajesh M R <rajesh.mr82@gmail.com>
2021-10-06 14:22:29 +0530

    Fixed formatting

--- test/test.js
+++ test/test.js
@@ -1,52 +1,52 @@
-const expect = require("chai").expect,
-  { getQuote, getRandomQuote } = require("../lib/index");
+const expect = require('chai').expect,
+  { getQuote, getRandomQuote } = require('../lib/index');
 
-allQuotes = require("../data/data.json");
+allQuotes = require('../data/data.json');
 
-describe("Running tests for methods of inspirational-quotes", () => {
-  describe("getQuote", () => {
-    it("should return an object", (done) => {
+describe('Running tests for methods of inspirational-quotes', () => {
+  describe('getQuote', () => {
+    it('should return an object', (done) => {
       const randomQuote = getQuote();
-      expect(randomQuote).to.be.an("object");
+      expect(randomQuote).to.be.an('object');
       done();
     });
-    it("should contain text and author", (done) => {
+    it('should contain text and author', (done) => {
       const randomQuote = getQuote();
-      expect(randomQuote).to.have.property("author");
-      expect(randomQuote).to.have.property("text");
+      expect(randomQuote).to.have.property('author');
+      expect(randomQuote).to.have.property('text');
       done();
     });
-    it("should not contain empty text", (done) => {
+    it('should not contain empty text', (done) => {
       const randomQuote = getQuote();
       expect(randomQuote.text).to.not.be.empty;
       done();
     });
-    it("should not contain empty author", (done) => {
+    it('should not contain empty author', (done) => {
       const randomQuote = getQuote();
       expect(randomQuote.author).to.not.be.empty;
       done();
     });
-    it("should not return author field if author: false is passed", (done) => {
+    it('should not return author field if author: false is passed', (done) => {
       const randomQuote = getQuote({ author: false });
-      expect(randomQuote).to.not.have.property("author");
-      expect(randomQuote).to.have.property("text");
+      expect(randomQuote).to.not.have.property('author');
+      expect(randomQuote).to.have.property('text');
       done();
     });
   });
-  describe("getRandomQuote", () => {
-    it("should return a string", (done) => {
+  describe('getRandomQuote', () => {
+    it('should return a string', (done) => {
       const randomQuote = getRandomQuote();
-      expect(randomQuote).to.be.an("string");
+      expect(randomQuote).to.be.an('string');
       done();
     });
-    it("should have length greater than 5", (done) => {
+    it('should have length greater than 5', (done) => {
       const randomQuote = getRandomQuote();
       expect(randomQuote).to.have.length.greaterThan(5);
       done();
     });
   });
-  describe("data.json", () => {
-    it("should not have any duplicate quotes", (done) => {
+  describe('data.json', () => {
+    it('should not have any duplicate quotes', (done) => {
       let duplicates = [
         ...allQuotes
           .reduce((accumulator, current) => {
